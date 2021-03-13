8ad26de397c5a746bd23b4b4ebdd414e8880cab3
Bartosz Mikulski <bartmiki97@gmail.com>
2021-03-13 13:43:02 +0100

    Video ID corrections

--- utils.py
+++ utils.py
@@ -1,5 +1,20 @@
 import json
+import googleapiclient.discovery
 
 def read_config(config_filename: str):
     with open(config_filename, 'r') as f:
         return json.load(f)
+
+
+class YTDownloader:
+  def __init__(self, apikey):
+    self.youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=apikey)
+
+  def get_id(self, title, channel):
+    request = self.youtube.search().list(
+        part="snippet",
+        maxResults=1,
+        q=f"{title} {channel}")
+    response = request.execute()
+    print(f'For {title} and {channel} the response is: {response}')
+    return response
