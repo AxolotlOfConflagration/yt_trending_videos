6065f6ea1693d7525a59aa67b2728da084daf598
Bartosz Mikulski <bartmiki97@gmail.com>
2021-04-10 11:26:16 +0200

    Downloaded YT categories

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import json
+import pickle
+import pandas as pd
 import googleapiclient.discovery
+
 
 def read_config(config_filename: str):
     with open(config_filename, 'r') as f:
@@ -7,14 +10,38 @@ def read_config(config_filename: str):
 
 
 class YTDownloader:
-  def __init__(self, apikey):
-    self.youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=apikey)
+    def __init__(self, apikey):
+        self.youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=apikey)
 
-  def get_id(self, title, channel):
-    request = self.youtube.search().list(
-        part="snippet",
-        maxResults=1,
-        q=f"{title} {channel}")
-    response = request.execute()
-    print(f'For {title} and {channel} the response is: {response}')
-    return response
+    def get_id(self, title, channel):
+        request = self.youtube.search().list(
+            part="snippet",
+            maxResults=1,
+            q=f"{title} {channel}")
+        response = request.execute()
+        print(f'For {title} and {channel} the response is: {response}')
+        return response
+
+    def details_by_id(self, idx):
+        request = self.youtube.videos().list(
+            part="snippet",
+            id=','.join(idx)
+        )
+        return request.execute()
+
+
+def video_id_without_categories(df):
+    return df[df.category_id.isna()].index
+
+
+if __name__ == "__main__":
+    df = pd.read_pickle('data/chkp1.pkl')
+    idx = video_id_without_categories(df)
+    config = read_config('config.json')
+    yt = YTDownloader(config['apikey'])
+
+    bsize = 10
+    results = [yt.details_by_id(idx[x*bsize:(x+1)*bsize]) for x in range((len(idx) // bsize)+1) ]
+
+    with open('data/category_id.pkl', 'wb') as fh:
+        pickle.dump(results, fh)
