bbf7a441a82740ccbc150d2aa2ee0ef73247b3df
Bartosz Mikulski <bartmiki97@gmail.com>
2021-05-04 20:04:39 +0200

    Downloading script

--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@ import json
 import pickle
 import pandas as pd
 import googleapiclient.discovery
+from googleapiclient.errors import HttpError
+
+from tinydb import TinyDB, Query
 
 
 def read_config(config_filename: str):
@@ -29,19 +32,57 @@ class YTDownloader:
         )
         return request.execute()
 
+    def find_related(self, idx, minDate="2006-07-23T08:24:11+00:00", maxDate="2018-06-14T01:31:53+00:00"):
+        request = self.youtube.search().list(
+            part="snippet",
+            maxResults=50,
+            publishedAfter=minDate,
+            publishedBefore=maxDate,
+            relatedToVideoId=idx,
+            type="video"
+        )
+        return request.execute()
+
 
 def video_id_without_categories(df):
     return df[df.category_id.isna()].index
 
 
 if __name__ == "__main__":
-    df = pd.read_pickle('data/chkp1.pkl')
-    idx = video_id_without_categories(df)
     config = read_config('config.json')
-    yt = YTDownloader(config['apikey'])
+    start_transaction = lambda: TinyDB('data/not_trending_db.json')
+    # yt = YTDownloader(config['apikey'])
+    yt = YTDownloader(config['apikey_alt'])
 
-    bsize = 10
-    results = [yt.details_by_id(idx[x*bsize:(x+1)*bsize]) for x in range((len(idx) // bsize)+1) ]
 
-    with open('data/category_id.pkl', 'wb') as fh:
-        pickle.dump(results, fh)
+    for _ in range(105):
+        Video = Query()
+        with start_transaction() as db:
+            doc = db.get(~(Video.related.exists()))
+            print("Query for:", doc['video_id'])
+            try:
+                doc['related'] = yt.find_related(doc['video_id'])
+                db.update(doc, Video.video_id == doc['video_id'])
+                
+            except HttpError as ex:
+                print(ex)
+                if (ex.resp['status'] == "404"):
+                    print(f"Updating {doc['video_id']} with empty string")
+                    doc['related'] = ""
+                    db.update(doc, Video.video_id == doc['video_id'])
+                else:
+                    print(ex.resp['status'], "exiting")
+                    exit(0)
+                
+            except Exception as ex:
+                print(ex)
+                exit(0)
+
+    # df = pd.read_pickle('data/chkp1.pkl')
+    # idx = video_id_without_categories(df)
+
+    # bsize = 10
+    # results = [yt.details_by_id(idx[x*bsize:(x+1)*bsize]) for x in range((len(idx) // bsize)+1) ]
+
+    # with open('data/category_id.pkl', 'wb') as fh:
+    #     pickle.dump(results, fh)
