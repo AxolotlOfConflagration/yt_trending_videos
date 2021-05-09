c7912aa36051f276f1a5b47a11c18b96272ee26c
Bartosz Mikulski <bartmiki97@gmail.com>
2021-05-09 19:50:33 +0200

    Add processing of image histogram, image statistics, face/expression recognition and text detection

--- download_details.py
+++ download_details.py
@@ -6,7 +6,7 @@ from googleapiclient.errors import HttpError
 
 from tinydb import TinyDB, Query
 from utils import read_config, YTDownloader
-
+from tqdm import tqdm
 
 def get_unique_ids(docs):
     results = []
@@ -17,11 +17,24 @@ def get_unique_ids(docs):
     return set(results)
 
 
+def to_dict(video):
+    results = {'video_id': video['id']}
+    results = {**results, **video['snippet'], **video['statistics']}
+    return results
+    
+    
 if __name__ == "__main__":
     config = read_config('config.json')
     start_transaction = lambda: TinyDB('data/not_trending_db.json')
     # yt = YTDownloader(config['apikey'])
     yt = YTDownloader(config['apikey_alt'])
+
+    with open('data/chkp5/data.json') as fh:
+        non_trending_movies = json.load(fh)
+        downloaded_ids = set(m['video_id'] for m in non_trending_movies)
+
+    with open('data/chkp5/data_old.json', 'w') as fh:
+        json.dump(non_trending_movies, fh)
 
     with start_transaction() as db:
         Video = Query()
@@ -31,30 +44,28 @@ if __name__ == "__main__":
         print(len(trending_ids))
         
         non_trending_ids = get_unique_ids(doc)
-        print(len(non_trending_ids))
-        print(len(non_trending_ids - trending_ids))
-        values = list(non_trending_ids - trending_ids)[:10]
-        print(values)
+        print("All ids", len(non_trending_ids))
+        non_trending_ids -= trending_ids
+        print("All non-trending ids", len(non_trending_ids))
+        non_trending_ids -= downloaded_ids
+        print("Non-trending ids do download", len(non_trending_ids))
+        non_trending_ids = list(non_trending_ids)
 
+    try:
+        bsize = 25
+        for x in tqdm(range((len(non_trending_ids) // bsize)+1)):
+            downloaded = yt.details_by_id(non_trending_ids[x*bsize:(x+1)*bsize])
+            non_trending_movies += [to_dict(d) for d in downloaded['items']]
 
-        # print("Query for:", doc['video_id'])
-        # try:
-        #     doc['related'] = yt.find_related(doc['video_id'])
-        #     db.update(doc, Video.video_id == doc['video_id'])
-            
-        # except HttpError as ex:
-        #     print(ex)
-        #     if (ex.resp['status'] == "404"):
-        #         print(f"Updating {doc['video_id']} with empty string")
-        #         doc['related'] = ""
-        #         db.update(doc, Video.video_id == doc['video_id'])
-        #     else:
-        #         print(ex.resp['status'], "exiting")
-        #         exit(0)
-            
-        # except Exception as ex:
-        #     print(ex)
-        #     exit(0)
+    except HttpError as ex:
+        print(ex)
+        print("Status code:", ex.resp['status'])
+    except Exception as ex:
+        print(ex)
+    finally:
+        print("Movies total:", len(non_trending_movies))
+        with open('data/chkp5/data.json', 'w') as fh:
+            json.dump(non_trending_movies, fh)
 
 
     # idx = video_id_without_categories(df)
